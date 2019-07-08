317a4293874f0457c8cf8f50ca478645f9f08ef1
DEAN HUANG (ITA-ISDC-ISD-OOCLL/ZHA) <HUANGDE4@OOCL.COM>
2019-07-08 11:51:35 +0800

    practice 1 es6 version

--- practice/section-1/practice-1.js
+++ practice/section-1/practice-1.js
@@ -1,19 +1,22 @@
 'use strict';
 
 function collectSameElements(collectionA, collectionB) {
-  let arr = [];
-  let hash = [];
-  for(let i = 0;i < collectionA.length;i++){
-    for(let j = 0;j < collectionB.length;j++){
-      if(collectionA[i] === collectionB[j]){
-        arr.push(collectionA[i]);
-      }
-    }
-  }
-  for(let i = 0;i < arr.length;i++){
-   if(hash.lastIndexOf(arr[i]) == -1){
-     hash.push(arr[i]);
-   }
-  }
-  return hash;
+  // let arr = [];
+  // let hash = [];
+  // for(let i = 0;i < collectionA.length;i++){
+  //   for(let j = 0;j < collectionB.length;j++){
+  //     if(collectionA[i] === collectionB[j]){
+  //       arr.push(collectionA[i]);
+  //     }
+  //   }
+  // }
+  // for(let i = 0;i < arr.length;i++){
+  //  if(hash.lastIndexOf(arr[i]) == -1){
+  //    hash.push(arr[i]);
+  //  }
+  // }
+  // return hash;
+
+  // es6 version
+  return collectionA.filter(item => collectionB.indexOf(item) !== -1)
 }
